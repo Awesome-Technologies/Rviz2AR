7fb68cc5c5fed64fbb5b06f7f97ef2bd87fe2cc6
Michael Rojkov <michael.rojkov@awesome-technologies.de>
2019-09-03 15:54:08 +0200

    Add functions to place interactive ur5 model in scene

---
+++ Assets/Scripts/Rviz2_scripts/PlaceUR5.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Buttons;
+using HoloToolkit.Unity.SpatialMapping;
+
+public class PlaceUR5 : MonoBehaviour
+{
+    private Button button;
+
+    public GameObject ur5;
+
+    private UR5TapToPlace placeUr5;
+
+    // Use this for initialization
+    void Start()
+    {
+        //ur5 = GameObject.Find("ur5Model");
+        placeUr5 = ur5.GetComponent<UR5TapToPlace>();
+    }
+
+    private void OnEnable()
+    {
+        button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.OnButtonClicked += OnButtonClicked;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnButtonClicked(GameObject obj)
+    {
+        Debug.Log("Ich bewege ur5");
+        if (ur5.active == false) {
+            ur5.active = true;
+            placeUr5.IsBeingPlaced = true;
+        } else
+        {
+            ur5.active = false;
+        }
+    }
+
+}
